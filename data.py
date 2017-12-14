2e877b1040c362db449eedfeb487f440ef4ba4ec
DieterLan <songnian.lan@student.uni-tuebingen.de>
2017-12-14 22:47:20 +0100

    why

--- data.py
+++ data.py
@@ -1,8 +1,3 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-
 import cv2
 from tensorpack import *
 import glob
@@ -15,18 +10,16 @@ import random
 from tensorpack import RNGDataFlow, MapDataComponent, dftools, FixedSizeData
 
 
+zip_folder = '/Users/apple/Downloads/Computergrafik/zipfile'
+#zip_folder = '/graphics/scratch/mallick/KITTI'
 
 
-# example_dir = '/home/wieschol/0default/Downloads/2011_09_26/2011_09_26_drive_0001_sync'
-# example_dir = '/Users/apple/Downloads/Computergrafik/2011_09_26/2011_09_26_drive_0001_sync'
-# zip_dir = '/Users/apple/Downloads/Computergrafik/2011_09_26_drive_0001_sync.zip'
-#zip_folder = '/Users/apple/Downloads/Computergrafik/zipfile'
-zip_folder = '/graphics/scratch/mallick/KITTI'
 class ImageEncode(MapDataComponent):
     def __init__(self, ds, mode='.jpg', dtype=np.uint8, index=0):
         def func(img):
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             return np.asarray(bytearray(cv2.imencode(mode, img)[1].tostring()), dtype=dtype)
+
         super(ImageEncode, self).__init__(ds, func, index=index)
 
 
@@ -35,38 +28,25 @@ class ImageDecode(MapDataComponent):
         def func(im_data):
             img = cv2.imdecode(np.asarray(bytearray(im_data), dtype=dtype), cv2.IMREAD_COLOR)
             return img
+
         super(ImageDecode, self).__init__(ds, func, index=index)
 
 
-# class KittiReader(RNGDataFlow):
-#     """docstring for KittiReader"""
-#     def __init__(self, example_dir):
-#         super(KittiReader, self).__init__()
-#         self.pattern = os.path.join(example_dir, 'image_02', 'data', '*.png')
-
-#     def get_data(self):
-
-#         image_names = glob.glob(self.pattern)
-
-#         for fn in image_names:
-#             left_img = cv2.imread(fn)
-#             right_img = cv2.imread(fn.replace('image_02', 'image_03'))
-#             yield [left_img, right_img]
-
 class KittiFromZIPFile(RNGDataFlow):
     """ Produce images read from a list of zip files. """
+
     def __init__(self, zip_folder, shuffle=False):
         """
         Args:
             zip_file (list): list of zip file paths.
         """
 
-        #assert os.path.isfile(zip_file)
+        # assert os.path.isfile(zip_file)
         self.archivefiles = []
         zip_list = [str for str in os.listdir(zip_folder) if 'drive' in str]
         # print zip_list
         for zip_file in zip_list:
-            zip_dir = os.path.join(zip_folder,zip_file)
+            zip_dir = os.path.join(zip_folder, zip_file)
             assert os.path.isfile(zip_dir)
             self.shuffle = shuffle
             archive = zipfile.ZipFile(zip_dir)
@@ -80,16 +60,13 @@ class KittiFromZIPFile(RNGDataFlow):
         if self.shuffle:
             self.rng.shuffle(self.archivefiles)
         # self.archivefiles = random.sample(self.archivefiles, self.max)
-        i=0
+        i = 0
         for archive in self.archivefiles:
             if ('image_02' in archive[1]):
                 left_img = archive[0].read(archive[1])
                 right_img = archive[0].read(archive[1].replace('image_02', 'image_03'))
 
                 yield [left_img, right_img]
-
-            # im_data = archive[0].read(archive[1])
-            # yield [left_imData,right_imData]
 
 
 def main():
@@ -98,10 +75,6 @@ def main():
     ds = FixedSizeData(ds, 500)
     dftools.dump_dataflow_to_lmdb(ds, 'train2.lmdb')
 
-    # read
-    # ds = LMDBDataPoint('train2.lmdb', shuffle=True)
-    # TestDataSpeed(ds).start()
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -109,3 +82,6 @@ if __name__ == '__main__':
     parser.add_argument('--apply', action='store_true', help='apply')
     args = parser.parse_args()
     main()
+
+
+
